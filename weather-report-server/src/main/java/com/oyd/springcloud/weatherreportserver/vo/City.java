e6e95b6fb2d277257655b82b9b9075cd1c666307
oydddddd <oyd31310@163.com>
2020-04-19 15:17:02 +0800

    删除无用的包

--- weather-report-server/src/main/java/com/oyd/springcloud/weatherreportserver/vo/City.java
+++ weather-report-server/src/main/java/com/oyd/springcloud/weatherreportserver/vo/City.java
@@ -2,11 +2,6 @@ package com.oyd.springcloud.weatherreportserver.vo;
 
 import lombok.Data;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-
 @Data
 public class City {
 
